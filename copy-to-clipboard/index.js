97e76abafdd3c105826e8d9151fc212ff88d315e
Shak Stick <sanshakurt@gmail.com>
2022-07-29 09:22:22 +0430

    solve some bugs

--- copy-to-clipboard/index.js
+++ copy-to-clipboard/index.js
@@ -1,6 +1,11 @@
-$("button").click(() => {
-  navigator.clipboard.writeText($("textarea").val());
-  $("p").show();
-  $("p").fadeIn(500);
-  $("p").fadeOut(750);
+$(document).ready(() => {
+  $("button").click(() => {
+    navigator.clipboard.writeText($("textarea").val());
+    // $("textarea").select();
+    // document.execCommand("copy");
+
+    $("p").fadeIn(400);
+    $("p").addClass("gelatine");
+    $("p").fadeOut(1000);
+  });
 });
